9fa08ef7bbb3acba9c7c65ce4bd39f4056b86d03
bobthefish29 <dapunchak@email.neit.edu>
2024-09-12 10:15:37 -0400

    myWork

--- pythonFiles/program.py
+++ pythonFiles/program.py
@@ -8,9 +8,24 @@ file = pd.read_csv("data/SeedUnofficialAppleDataCSV.csv")
 #when you want to view different data you would have to do the 'unnamed: #' because the data is poop. 
 # It is Operating system support(model),relesed OS,Data, discontinued,support end,Final os,lifespan,min,price 
 # 
+file.dropna(subset='Operating system support', inplace=True)
 
-print(file['Unnamed: 1'])
+# print(file['Operating system support'])
 
+phoneList = []
+phoneList2 = []
+
+for i in file['Operating system support']:
+    
+    phoneList.append(i)
+
+
+for i in range(0,len(phoneList)-1 ):
+
+    print(phoneList[i+1].split("/"))
+    phoneList2.append(phoneList[i+1].split("/"))
+
+print(phoneList2[8][1])
 # print(file['Unnamed: 2'])
 
 
