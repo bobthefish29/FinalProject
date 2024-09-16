0da7fb17aa9fdb909384448b1b2c9110d71c2fbb
bobthefish29 <dapunchak@email.neit.edu>
2024-09-16 19:04:21 -0400

    FinishedTheDataFrame

--- pythonFiles/Iphone_VS_price.py
+++ pythonFiles/Iphone_VS_price.py
@@ -42,8 +42,11 @@ import numpy as np
 
 '''
 
-file = pd.read_csv(r'data/SeedUnofficialAppleDataCSV.csv',encoding="UTF-8")#1
+file = pd.read_csv(r'data/SeedUnofficialAppleDataCSV.csv', encoding="UTF-8")#1
 file.columns = ['model', 'release_os', 'release_date', 'discontinued', 'support_ended','final_os', 'lifespan', 'max_lifespan', 'launch_price']#2
+
+
+
 
 #so the next thing i need to look into is getting the dates
 
@@ -80,17 +83,40 @@ def getPhoneList():
     return phoneList2
 
 #this is where i am playing around with dates
-def getRLDateList():
+def getRDateList():
     tfile = file.dropna(subset='release_date', inplace=False, axis='index')
 
-    date_string = tfile['release_date']
-    date_object = pd.to_datetime(date_string, format="%B %d, %Y")
+    date_object = []
+    for i in tfile['release_date']:
+        date_object.append(i.replace('�', ' '))
 
-    print(date_object)
 
-    print(tfile['release_date'])
+    for i in range(1,len(date_object)):
+        # i.split("/")
+        print('before',date_object[i])
+        # date_object[i] = date_object[i].split("/")
+        print(date_object[i])
 
-getRLDateList()
+
+        # if(len(date_object[i]) > 1):
+        #     for l in range(0,len(date_object[i])):
+        #         date_object[i].append(date_object[i][l])
+        # print('\nafter',date_object[i],'\n')
+
+
+    print(len(date_object))
+
+    
+
+    # print(tfile['release_date'].replace('June', ' hi'))
+    #�
+    date_object2 = pd.to_datetime(date_object, format="%B %d, %Y", errors='coerce')
+
+    print(date_object2)
+
+    # print(tfile['release_date'])
+
+# getRDateList()
 
 # print('There are',len(getPhoneList()),'Iphones\n\n')
 
@@ -190,12 +216,156 @@ def getPriceList():
 
     return df
 
+
+
+#this is the one that will include the price
+def getPriceListWithYear():
+    #['launch_price'].dropna(inplace=False, axis='index')
+
+    #print('\n\t--In the price function---\n')
+    tempFile = file
+    result_list = []
+    realFinal = []
+    date_object = []
+
+
+    #this is adding all the values and stuff together
+    #what its doing it looking at the model row, if the value is null than it would add the lanch price to a list of the item before
+    #if its not null than it would add the value to the list
+    for i, r in tempFile.iterrows():
+        if pd.isnull(r['model']):
+            # Add the launch_price to the last non-null model's list
+            result_list[-1][1].append(r['launch_price'])
+        else:
+            # Create a new entry for the non-null model
+            result_list.append([r['model'], [r['launch_price']],r["release_date"]])
+            # date_object.append(r["release_date"])
+        
+    # dateTemp = file.dropna(subset='release_date', inplace=False, axis='index')
+    # for i in range(1,len(dateTemp)):
+    #     date_object.append(dateTemp)
+    #     #date = pd.to_datetime(r["release_date"], format="%B %d, %Y", errors='coerce')
+    #     # date_object.append(pd.to_datetime(i, format="%B %d, %Y", errors='coerce'))
+
+    # print(len(date_object))
+    # print(len(result_list))
+
+
+
+
+    #this is for a lot ngl, its first looping through each row
+    for i in range(1,len(result_list)):
+        #this is for the first row, if there is a "/" it will spice it, if not it will just add it to its own list
+        #if it does find a "/" it replace it with a space
+        result_list[i][0] = result_list[i][0].split("/")
+
+
+        result_list[i][2] = result_list[i][2].split("/")
+        #this is than looking if the item in the firs list has a " " than it puts iphone.
+        for l in range(0, len(result_list[i][0])):
+            if result_list[i][0][l].startswith(" "):
+                #print(result_list[i][0][l])
+                result_list[i][0][l] = "iphone" + result_list[i][0][l]
+            #this is removing the thing
+            result_list[i][2][0] = result_list[i][2][0].replace('�', ' ')
+        #this is looking at the price list, its each item in the price list
+        #if it has a "*" than it removes it, if it has a "$" it removes it
+        for l in range(0,len(result_list[i][1])):
+            # print(result_list[i][1][l].replace('$', ''))
+            result_list[i][1][l] = result_list[i][1][l].replace('*', '')
+            result_list[i][1][l] = result_list[i][1][l].replace('$', '')
+            result_list[i][1][l] = result_list[i][1][l].replace('Plus:', '')
+            result_list[i][1][l] = result_list[i][1][l].replace('Max:', '')
+            result_list[i][1][l] = result_list[i][1][l].replace('Mini:', '')
+
+        if len(result_list[i][2]) == 2:
+            result_list[i][2][1] = result_list[i][2][1].replace(' (12 Mini)', '')
+            result_list[i][2][1] = result_list[i][2][1].replace(' (12 Pro Max)', '')
+            result_list[i][2][1] = result_list[i][2][1].replace(' November', 'November')
+
+
+        result_list[i][2][0] = result_list[i][2][0].replace(' (12 Pro)', '')
+        result_list[i][2][0] = result_list[i][2][0].replace(' (12)', '')
+
+        result_list[i][2][0] = result_list[i][2][0].replace('2020 ', '2020')
+
+
+
+
+
+        
+
+
+    # print('for\n\n\n\n\n')
+    # for i in range(0, len(result_list)):
+    #     print(result_list[i])
+    # print('\n\n\n\n\n')
+
+
+    for i in range(1,len(result_list)):
+        if len(result_list[i][0]) == 1:#if the first list only has one item than it will do this
+            #print(f"---only one[1][]: {result_list[i]}--")
+            if len(result_list[i][1]) > 1:
+                #print(f"more than 1[1][2]: {result_list[i][1]}")
+                #realFinal.append([result_list[i][0][0],[result_list[i][1][0], result_list[i][1][1]]])
+                realFinal.append([result_list[i][0][0],result_list[i][1][0],result_list[i][1][1],result_list[i][2][0]])
+                #print([result_list[i][0][0],result_list[i][1][0],result_list[i][1][1],result_list[i][2][0]])
+            else:
+                #print(f"less than 1[1][1]: {result_list[i][1]}")
+                realFinal.append([result_list[i][0][0],result_list[i][1][0],'nun',result_list[i][2][0]])
+                #print([result_list[i][0][0],result_list[i][1][0],'null',result_list[i][2]])
+            #print('\n')
+        else:
+            #print(f"---more than one[2][]: {result_list[i]}")
+            if len(result_list[i][1]) > 2:
+                #print(f"more than two[2][4]: {result_list[i][l]}")
+                for l in range(0, len(result_list[i][0])):
+                    #print(f"loop {l}")
+                    if l == 0:
+                        realFinal.append([result_list[i][0][l],result_list[i][1][0],result_list[i][1][1],result_list[i][2][0]])
+                        #print([result_list[i][0][l],[result_list[i][1][0],result_list[i][1][1]],result_list[i][2]])
+                    else:
+                        realFinal.append([result_list[i][0][l],result_list[i][1][2],result_list[i][1][3],result_list[i][2][0]])
+                        #print([result_list[i][0][l],[result_list[i][1][2],result_list[i][1][3]],result_list[i][2][0]])
+                #print('\n')
+            else:
+                for l in range(0, len(result_list[i][0])):
+                    #print(f"loop {l}")
+                    if l == 0:
+                        realFinal.append([result_list[i][0][l],result_list[i][1][0],'nun',result_list[i][2][0]])
+                        #print([result_list[i][0][l],result_list[i][1][0],'nun',result_list[i][2][0]])
+                    else:
+                        if(result_list[i][0][l] == 'iphone 12 Mini' or result_list[i][0][l] == 'iphone 12 Pro Max'):
+                            #print([result_list[i][0][l],[result_list[i][1][1]],'nun',result_list[i][2][1]])
+                            realFinal.append([result_list[i][0][l],result_list[i][1][1],'nun',result_list[i][2][1]])
+                        else:
+                            #print([result_list[i][0][l],[result_list[i][1][1]],'nun',result_list[i][2][0]])
+                            realFinal.append([result_list[i][0][l],result_list[i][1][1],'nun',result_list[i][2][0]])
+
+    #this is converting the whole thing i just made into a data frame
+
+    
+    df = pd.DataFrame(realFinal, columns=['iphone', 'price', 'plane','year'])
+
+    df['year'] = df['year'].replace('', '')
+    df['year'] = pd.to_datetime(df['year'], format="%B %d, %Y", errors='coerce')
+
+    df['price'] = df['price'].str.split("/")
+    df['plane'] = df['plane'].str.split("/")
+
+    return df
+
     
 
 #this is where we will be adding the date to the dataFrame,
-priceDf = getPriceList()
+priceDf = getPriceListWithYear()
+otherDf = getPriceList()
 
-# print(priceDf)
+print('\n')
+print(priceDf)
+print('\n')
+print(otherDf)
+
 
 
 '''
@@ -302,8 +472,6 @@ def makeBasicTable(xlist,ylist):
 
     # Function to calculate slope and intercept
     def best_fit_slope_and_intercept(xs, ys):
-        print(((mean(xs) * mean(ys)) - mean(xs * ys)))
-        print(((mean(xs) * mean(xs)) - mean(xs * xs)))
         m = (((mean(xs) * mean(ys)) - mean(xs * ys)) / ((mean(xs) * mean(xs)) - mean(xs * xs)))
         #this is the y int value
         b = mean(ys) - m * mean(xs)
@@ -445,9 +613,12 @@ def makeTableWithYear(xlist,ylist, year):
 
 
 #this is a working version of the table
-# y = [int(x[0]) for x in priceDf['price']]  # First price list
-# x = [int(x[0]) for x in priceDf['price']]  # first price list
-# makeBasicTable(x,y)
+y = [int(x[0]) for x in priceDf['price']]  # First price list
+x = [int(x[0]) for x in priceDf['price']]  # first price list
+makeBasicTable(x,y)
+
+
+
 
 
 # y = [int(x[0]) for x in priceDf['price']]  # First price list
